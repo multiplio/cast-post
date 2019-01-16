3c921d047fec9a1a32fde6dac5184aac5292bbbe
WhoMeNope <whomenope@outlook.com>
2019-01-16 17:11:16 +0100

    initial

---
+++ index.js
@@ -0,0 +1,42 @@
+const logger = require('./logger')
+
+// app requires
+const users = require('./user')
+
+const app = require('express')()
+const session = require('express-session')
+
+app.use(require('helmet')())
+
+app.use(require('body-parser').urlencoded(
+  { extended: true }
+))
+
+// setup session store
+require('./sessstore')(session)
+  .then(store => {
+    // use the store
+    app.use(session(
+      {
+        secret: process.env.COOKIE_SECRET,
+        store: store,
+        resave: true,
+        saveUninitialized: true,
+        cookie: {
+          // secure: true,
+          maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+        },
+      }
+    ))
+
+    // setup users database
+    users()
+      .then(User => {
+        // setup paths
+
+        // start server
+        app.listen(process.env.PORT)
+        logger.info(`listening at localhost:${process.env.PORT}`)
+      })
+  })
+
