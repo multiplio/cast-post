6e99e511eede5e285e8fc50992259526a53e8832
Martin <whomenope@outlook.com>
2019-02-25 12:12:57 +0100

    fix save post to user db (#3)
    * fix save post to user db
    
    * lint + fix

--- post.js
+++ post.js
@@ -1,10 +1,16 @@
 const logger = require('./logger')
 const user = require('./user')
 const fetch = require('node-fetch')
+const mongoose = require('mongoose')
 
 const ipfs = require('ipfs-http-client')(process.env.IPFS_ADDRESS, process.env.IPFS_PORT, { protocol: process.env.IPFS_PROTOCOL })
 
 module.exports = (userID, post) => new Promise(function (resolve, reject) {
+  if (!mongoose.Types.ObjectId.isValid(`${userID}`)) {
+    reject(new Error(`invalid userID : ${userID}`))
+  }
+  const userOID = new mongoose.Types.ObjectId(`${userID}`)
+
   const postString = JSON.stringify(post)
   const buf = Buffer.from(postString, 'utf8')
 
@@ -30,8 +36,8 @@ module.exports = (userID, post) => new Promise(function (resolve, reject) {
     // add post to user
       .then(User => {
         User.update(
-          { _id: userID },
-          { $push: { posts: post } }
+          { '_id': userOID },
+          { '$push': { 'posts': post } }
         )
       })
     // route to publishers
